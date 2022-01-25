7afd57326f4a37735bd162a778571e6f9420a3d4
AlexVCS <alexcurtisslep@gmail.com>
2022-01-25 17:49:07 -0500

    have it working for one date and need to get it working for any date

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+const port = 3005;
+
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
-
-// your first API endpoint... 
+ 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+let date_string = "June 20, 1993"
+
 app.get("/api/:date?", time = (req, res, next) => {
-    req.time = new Date.toString()
+    req.time = new Date(date_string),
     next()
   }, function (req, res) {
-    res.json({"time": req.time})
+    res.json({
+      "unix": req.time.getTime(),
+      "utc": req.time.toString()
+    })
   }
 )
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+//if statement for if the date is an invalid string
+// if req.time === null,
+// }, function (req, res) {
+  // res.json({
+  //   error: "Invalid Date"
+  // })
+
+var listener = app.listen(port, function () {
+  console.log(`Your app is listening on port ${port}`);
 });
