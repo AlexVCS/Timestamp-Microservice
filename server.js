82ec0977df3f75634529134a9f846e5fa47e4170
AlexVCS <alexcurtisslep@gmail.com>
2022-02-10 19:38:35 -0500

    Added regex for checking valid date strings with dashes.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 const port = 3005;
 
 var cors = require('cors');
+const { json } = require('express/lib/response');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 app.use(express.static('public'));
@@ -17,27 +18,34 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// create a helper function that checks for if it's a valid date
+// const checkForValidDate = 
+
+
+
 app.get("/api/:date?",  function (req, res, next) {
-  if (req.params.date?.includes("-")) {
+  if (!req.params.date) {
+    req.params.date = Date.now()
+  } else if (/^\d{4}-\d{2}-\d{2}$/) {
     req.params.date = Date.parse(`${req.params.date} 00:00:00`)
-  } else {
-    req.params.date = parseInt(req.params.date)
   }
+  // else if ( // check if unix date is valid
+  //   ) {
+  //   req.params.date = parseInt(req.params.date)
+  // } else {
+  //   // send res for invalid date
+      // res.json({
+      // "error": "Invalid Date"
+      // })
+  // }
   next()
 }, function (req, res) {
   let date = new Date(req.params.date);
-  res.json({
-    "unix": date.valueOf(),
-    "utc": date.toUTCString()
-  })
+    res.json({
+      "unix": date.valueOf(),
+      "utc": date.toUTCString()
+    })
 })
-
-//if statement for if the date is an invalid string
-// if req.time === null,
-// }, function (req, res) {
-  // res.json({
-  //   error: "Invalid Date"
-  // })
 
 app.listen(PORT, function () {
   console.log(`Your app is listening on port ${PORT}`);
