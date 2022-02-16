f04877b5b373baa0c5e23003324d1e2b265b6af3
Alex Curtis-Slep <alexcurtisslep@Alexs-MacBook-Air.local>
2022-02-16 09:27:01 -0500

    refactoring with helper functions, proper logic to make the output work for any valid input and through an error for invalid inputs

--- server.js
+++ server.js
@@ -18,17 +18,65 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-// create a helper function that checks for if it's a valid date
-// const checkForValidDate = 
+function isNumbersOnly(string) {
+  return /^\d+$/.test(string)
+}
 
-
-
-app.get("/api/:date?",  function (req, res, next) {
-  if (!req.params.date) {
-    req.params.date = Date.now()
-  } else if (/^\d{4}-\d{2}-\d{2}$/) {
-    req.params.date = Date.parse(`${req.params.date} 00:00:00`)
+function makeDate(string) {
+  if (isNumbersOnly(string)) {
+    return new Date(parseInt(string))
+  } else {
+    return new Date(string)
   }
+}
+
+app.get("/api/:date?",  function (req, res) {
+  let dateObject = null
+  let requestDate = req.params.date
+  if (!requestDate) {
+    dateObject = new Date()
+  } else {
+    dateObject = makeDate(requestDate)
+  }
+
+  if(isNaN(dateObject.valueOf())) {
+    res.json({
+      "error": "Invalid Date"
+    })
+  }
+
+  res.json({
+    "unix": dateObject.valueOf(),
+    "utc": dateObject.toUTCString()
+  })
+})
+
+app.listen(PORT, function () {
+  console.log(`Your app is listening on port ${PORT}`);
+});
+
+
+// app.get("/api/:date?",  function (req, res, next) {
+//   if (!req.params.date) {
+//     req.params.date = myDate
+//   } else if (/^\d{4}-\d{2}-\d{2}$/.test(req.params.date)) {
+//     req.params.date = Date.parse(`${req.params.date} 00:00:00`)
+//   }
+  // else if (myDate === null) {
+  //   res.json({
+  //     "error": "Invalid Date"
+  //   })
+  // }
+  // else {
+  //   const unixDate = new Date(parseInt(req.params.date))
+  //   console.log(unixDate.valueOf());
+  //   if(isNaN(unixDate.valueOf())) {
+  //     res.json({
+  //       "error": "Invalid Date"
+  //     })
+  //   }
+  //   req.params.date = parseInt(req.params.date)
+  // }
   // else if ( // check if unix date is valid
   //   ) {
   //   req.params.date = parseInt(req.params.date)
@@ -38,15 +86,11 @@ app.get("/api/:date?",  function (req, res, next) {
       // "error": "Invalid Date"
       // })
   // }
-  next()
-}, function (req, res) {
-  let date = new Date(req.params.date);
-    res.json({
-      "unix": date.valueOf(),
-      "utc": date.toUTCString()
-    })
-})
-
-app.listen(PORT, function () {
-  console.log(`Your app is listening on port ${PORT}`);
-});
+//   next()
+// }, function (req, res) {
+//   let date = new Date(req.params.date);
+//     res.json({
+//       "unix": date.valueOf(),
+//       "utc": date.toUTCString()
+//     })
+// })
